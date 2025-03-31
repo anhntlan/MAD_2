b8514c5de3284dadffe101770671a7560563424d
anhntlan <ntla1813@gmail.com>
2025-03-31 15:09:53 +0700

    first commit

---
+++ app/src/main/java/com/example/hipenjava/NotificationActivity.java
@@ -0,0 +1,17 @@
+package com.example.hipenjava;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
