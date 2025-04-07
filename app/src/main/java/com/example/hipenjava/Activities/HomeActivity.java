4bd52f43c784bd3d3bd4f477a23a82d70a418ee8
anhntlan <ntla1813@gmail.com>
2025-04-07 17:12:35 +0700

    fix bottom navigation in home, auth using firebase

--- app/src/main/java/com/example/hipenjava/Activities/HomeActivity.java
+++ app/src/main/java/com/example/hipenjava/Activities/HomeActivity.java
@@ -7,14 +7,20 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.hipenjava.Activities.Courses.CourseListActivity;
 import com.example.hipenjava.Activities.Notification.NotificationActivity;
 import com.example.hipenjava.Activities.BaseActivity;
 import com.example.hipenjava.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class HomeActivity extends BaseActivity {
-    private ImageButton btnNotification;
+    private ImageButton btnNotification,btnMenu;
+    BottomNavigationView bottomNavigation;
 
 
     @Override
@@ -22,17 +28,18 @@ public class HomeActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         // both header, bottom navigation
+        bottomNavigation = findViewById(R.id.bottomNavigation);
         getLayoutInflater().inflate(R.layout.activity_main, findViewById(R.id.contentFrame));
 
-//        btnNotification = findViewById(R.id.btnNotification);
-//
-//        btnNotification.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(HomeActivity.this, NotificationActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        btnNotification = findViewById(R.id.btnNotification);
+
+        btnNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
 
         LinearLayout navigationCourses = findViewById(R.id.navigation_courses_home);
 
@@ -43,6 +50,27 @@ public class HomeActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+        bottomNavigation.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.navigation_courses) {
+                Intent intent = new Intent(HomeActivity.this, CourseListActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (id == R.id.navigation_draw) {
+                Intent intent = new Intent(HomeActivity.this, CourseListActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (id == R.id.navigation_challenge) {
+                Intent intent = new Intent(HomeActivity.this, CourseListActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (id == R.id.navigation_community) {
+                Intent intent = new Intent(HomeActivity.this, CourseListActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        });
     }
 
-    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//plugins {
+//    id("com.android.application") version "7.4.2" apply false
+//
+//    // Add the dependency for the Google services Gradle plugin
+//    id("com.google.gms.google-services") version "4.4.2" apply false
+//
+//}
+//buildscript {
+//    dependencies {
+//        classpath ("com.google.gms:google-services:4.4.2")
+//
+//    }
+//}after lỗi sdk35 => null exception
 plugins {
-    alias(libs.plugins.android.application) apply false
+    id("com.android.application") version "8.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
